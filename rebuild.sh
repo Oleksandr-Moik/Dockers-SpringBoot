dc16ddee144a748ab19e6016c86fa7835e3e3393
Virus_Mix <sashamoik2016@gmail.com>
2019-11-12 14:17:37 +0200

    wrong ports and build path fixed

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
-cd ./DockerSBServise3_Age/ && gradle clean build && docker build . -t service-1-age:v1 &
+cd ./DockerSBServise3_Age/ && gradle clean build && docker build . -t service-3-age:v1 &
 cd ./DockerSBServise2_LastName/ && gradle clean build && docker build . -t service-2-lastname:v1 &
-cd ./DockerSBServise1_FirstName/ && gradle clean build && docker build . -t service-3-firsname:v1
+cd ./DockerSBServise1_FirstName/ && gradle clean build && docker build . -t service-1-firsname:v1
